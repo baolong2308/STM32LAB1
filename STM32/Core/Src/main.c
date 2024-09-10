5d876eaf381638e290013c39545912310d4dcf9f
BaoLong <phanhabaolong@gmail.com>
2024-09-10 22:42:34 +0700

    update Ex9

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -91,66 +91,67 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  void displayLed(int num){
+  void clearNumberOnClock(int num){
 	  switch(num){
 	  case 1:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
 		  break;
 	  case 2:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, RESET);
 		  break;
 	  case 3:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
 		  break;
 	  case 4:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
 		  break;
 	  case 5:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
 		  break;
 	  case 6:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
 		  break;
 	  case 7:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
 		  break;
 	  case 8:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
 		  break;
 	  case 9:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
 		  break;
 	  case 10:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
 		  break;
 	  case 11:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+
+		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
 		  break;
-	  case 12:
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
-		  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+	  case 0:
+
+		  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
 		  break;
 	  default:
 		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
 	  }
   }
-  int count=0;
+  //bật hết đèn để kiểm
+  //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , SET);
   while (1)
   {
     /* USER CODE END WHILE */
-	  if (count > 12) count=1;
-	  displayLed(count++);
+
+	  clearNumberOnClock(5);
 	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
