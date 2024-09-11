b0a8616c6eaf14550753cd292fbf29cca9de99a6
BaoLong <phanhabaolong@gmail.com>
2024-09-11 09:46:55 +0700

    update Ex8

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -137,7 +137,7 @@ int main(void)
 		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
 		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
 		  break;
-	  case 12:
+	  case 0:
 		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All , RESET);
 		  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
 		  break;
